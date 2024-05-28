de0ee9b6788844a0c5282242874f39d826ede5e5
seongguk <live8681@bigglz.com>
2024-05-28 14:37:10 +0900

    project copy

---
+++ programmersExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main()
+    {
+        // 리스트를 적절히 초기화
+        List<int> testListInt = new List<int> { 5, 2, 7, 5  };
+        int n = 3;
+        // 리스트를 배열로 변환하여 solution 메서드 호출
+        int[] result = solution(testListInt.ToArray(), n);
+    }
+    public static int[] solution(int[] num_list, int n)
+    {
+        int[] answer = new int[] { };
+        return answer;
+    }
+}
